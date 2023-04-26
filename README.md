eb0323e84e51fa3619fe20a27b129e4eb66ff7d0
Rajayne <ryann.j.nealey@gmail.com>
2023-04-26 16:48:52 -0700

    Create register method on User model

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 Install requirements and make Git repo
 Create messagely database and import schema from data.sql
+
+# Step 1
+
+Finish methods for user model
+
+- Create error handling
+- Run tests
--- models/user.js
+++ models/user.js
@@ -1,61 +1,51 @@
-/** User class for message.ly */
-
-
-
-/** User of the site. */
+const db = require("../db");
+const ExpressError = require("../expressError");
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR, SECRET_KEY } = require("../config");
 
 class User {
-
   /** register new user -- returns
-   *    {username, password, first_name, last_name, phone}
-   */
-
-  static async register({username, password, first_name, last_name, phone}) { }
+   *    {username, password, first_name, last_name, phone}*/
+  static async register({ username, password, first_name, last_name, phone }) {
+    const hashPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const newUser = await db.query(
+      `INSERT INTO users (
+        username, 
+        password, 
+        first_name, 
+        last_name, 
+        phone,
+        joine_at,
+        last_login_at) 
+      VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp) RETURNING username`,
+      [username, hashPassword, first_name, last_name, phone]
+    );
+    return newUser.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
-
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {}
 
   /** Update last_login_at for user */
-
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {}
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
-
-  static async all() { }
+  static async all() {}
 
   /** Get: get user by username
-   *
-   * returns {username,
-   *          first_name,
-   *          last_name,
-   *          phone,
-   *          join_at,
-   *          last_login_at } */
-
-  static async get(username) { }
+   * returns {username, first_name, last_name, phone, join_at, last_login_at } */
+  static async get(username) {}
 
   /** Return messages from this user.
-   *
    * [{id, to_user, body, sent_at, read_at}]
-   *
-   * where to_user is
-   *   {username, first_name, last_name, phone}
-   */
-
-  static async messagesFrom(username) { }
+   * where to_user is {username, first_name, last_name, phone} */
+  static async messagesFrom(username) {}
 
   /** Return messages to this user.
-   *
    * [{id, from_user, body, sent_at, read_at}]
-   *
-   * where from_user is
-   *   {username, first_name, last_name, phone}
-   */
-
-  static async messagesTo(username) { }
+   * where from_user is {username, first_name, last_name, phone} */
+  static async messagesTo(username) {}
 }
-
 
 module.exports = User;
