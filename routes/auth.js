2e4e9cfc2688e157d30705bb9c47e33874993818
Rajayne <ryann.j.nealey@gmail.com>
2023-04-26 17:01:32 -0700

    Create post route for auth to register new user

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,19 @@
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const router = new express.Router();
+
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config");
+const ExpressError = require("../expressError");
+
 /** POST /login - login: {username, password} => {token}
  * Make sure to update their last-login! */
 
 /** POST /register - register user: registers, logs in, and returns token.
  * {username, password, first_name, last_name, phone} => {token}.
  *  Make sure to update their last-login! */
+router.post("/register", async (res, req, next) => {
+  const { username } = await User.register(req.body);
+  let token = jwt.sign({ username }, SECRET_KEY);
+  res.json({ token });
+});
